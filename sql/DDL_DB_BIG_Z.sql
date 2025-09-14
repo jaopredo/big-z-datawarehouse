34d71a2f60fa8e748665b98a3b6129dc55de3183
jaopredo <joao.profissional.45@gmail.com>
2025-09-14 20:56:13 -0300

    Atualizando o DDL

--- sql/DDL_DB_BIG_Z.sql
+++ sql/DDL_DB_BIG_Z.sql
@@ -13,7 +13,7 @@ CREATE TABLE big_z.supplier (
 CREATE TABLE big_z.depot (
     DepotID SERIAL PRIMARY KEY,
     DepotSize INT NOT NULL,
-    DepotZip TEXT NOT NULL,
+    DepotZip TEXT NOT NULL
 );
 
 CREATE TABLE big_z.product (
@@ -22,4 +22,38 @@ CREATE TABLE big_z.product (
     ProductType TEXT NOT NULL,
     SupplierID INT NOT NULL,
     FOREIGN KEY (SupplierID) REFERENCES big_z.supplier(SupplierID)
+);
+
+CREATE TABLE big_z.orderclerk (
+    OCID SERIAL PRIMARY KEY,
+    OCName TEXT NOT NULL
+);
+
+CREATE TABLE big_z.customer (
+    CustomerID SERIAL PRIMARY KEY,
+    CustomerName TEXT NOT NULL,
+    CustomerType TEXT NOT NULL,
+    CustomerZip TEXT NOT NULL
+);
+
+CREATE TABLE big_z.order (
+    OrderID SERIAL PRIMARY KEY,
+    OrderDate DATE NOT NULL,
+    OrderTime TIME NOT NULL,
+    CustomerID INT NOT NULL,
+    DepotID INT NOT NULL,
+    OCID INT NOT NULL,
+
+    FOREIGN KEY (CustomerID) REFERENCES big_z.customer(CustomerID),
+    FOREIGN KEY (DepotID) REFERENCES big_z.depot(DepotID),
+    FOREIGN KEY (OCID) REFERENCES big_z.orderclerk(OCID)
+);
+
+CREATE TABLE big_z.orderedvia (
+    ProductID INT NOT NULL,
+    OrderID INT NOT NULL,
+    Quantity INT NOT NULL,
+
+    FOREIGN KEY (ProductID) REFERENCES big_z.product(ProductID),
+    FOREIGN KEY (OrderID) REFERENCES big_z.order(OrderID)
 );
